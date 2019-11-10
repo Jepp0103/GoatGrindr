85cc715137eae24c73c4accfe23055f8eb4c6139
Jeppe N. Dyekjær <jedy@itu.dk>
2019-11-10 14:31:50 +0100

    Trying to modify html page. Query added in Matches

--- src/main/java/edu/kea/jnd/goatsite/controller/api/MatchApiController.java
+++ src/main/java/edu/kea/jnd/goatsite/controller/api/MatchApiController.java
@@ -1,18 +1,13 @@
 package edu.kea.jnd.goatsite.controller.api;
 
-<<<<<<< HEAD
 import edu.kea.jnd.goatsite.model.Match;
-=======
 
->>>>>>> a0f2aa8e72608f50baae8996d1a3a3d2af2475f7
 import edu.kea.jnd.goatsite.repository.MatchRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-<<<<<<< HEAD
 import org.springframework.web.bind.annotation.RequestParam;
-=======
->>>>>>> a0f2aa8e72608f50baae8996d1a3a3d2af2475f7
+
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -20,7 +15,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class MatchApiController {
 
     @Autowired
-<<<<<<< HEAD
     private MatchRepository matchRepository;
 
     @GetMapping("/matches")
@@ -32,10 +26,5 @@ public class MatchApiController {
     public Iterable<Match> getMatchesByGoatId(@RequestParam(value = "id") Long id){
         return matchRepository.findAllByGoatId(id);
     }
-=======
-    MatchRepository matchRepository;
 
-    //@GetMapping
-
->>>>>>> a0f2aa8e72608f50baae8996d1a3a3d2af2475f7
 }
