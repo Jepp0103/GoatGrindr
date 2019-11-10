8eb73df70f764796f82f99b77f8c8f727ec8c0d4
Jeppe N. Dyekjær <jedy@itu.dk>
2019-11-10 14:35:24 +0100

    Updated GoatGrindr a little more

--- src/main/java/edu/kea/jnd/goatsite/repository/MatchRepository.java
+++ src/main/java/edu/kea/jnd/goatsite/repository/MatchRepository.java
@@ -1,17 +1,12 @@
 package edu.kea.jnd.goatsite.repository;
-<<<<<<< HEAD
-import edu.kea.jnd.goatsite.model.Goat;
-import edu.kea.jnd.goatsite.model.Match;
-import org.springframework.data.jpa.repository.Query;
-=======
 
 import edu.kea.jnd.goatsite.model.Match;
->>>>>>> a0f2aa8e72608f50baae8996d1a3a3d2af2475f7
+import org.springframework.data.jpa.repository.Query;
+
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-<<<<<<< HEAD
 public interface MatchRepository extends CrudRepository<Match, Long> {
     Iterable<Match> findAllByGoat1Id(Long goat1Id);
     Iterable<Match> findAllByGoat2Id(Long goat2Id);
@@ -22,12 +17,5 @@ public interface MatchRepository extends CrudRepository<Match, Long> {
 
     /*@Query(value = "SELECT * FROM Goats g WHERE g.id < 2", nativeQuery = true)
     Iterable<Goat> findTheOldTimers();*/
-=======
-public interface MatchRepository {
-
-    public interface DislikeRepository extends CrudRepository<Match, Long> {
-        Iterable<Match> findAllMatches(long goatMatches);
-    }
->>>>>>> a0f2aa8e72608f50baae8996d1a3a3d2af2475f7
 
 }
